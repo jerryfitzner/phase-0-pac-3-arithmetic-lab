230026ce695ee8d52f04524c794fa12a8aa78c67
Jerry Fitzner <jfitzner1@gmail.com>
2021-12-23 16:15:31 -0600

    Completted assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+function add(a, b){
+    return (a + b);
+}
+
+function subtract(a, b){
+    return (a - b);
+}
+
+function multiply(a, b){
+    return (a * b);
+}
+
+function divide(a, b){
+    return (a/b);
+}
+
+function increment(a){
+    return (a + 1)
+}
+
+function decrement(a){
+    return (a - 1)
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+add(800,21);
+subtract(654,1)
+multiply(55491,12)
+divide(0.04012036108324975*1000)
+increment(a)
+decrement(a)
+
